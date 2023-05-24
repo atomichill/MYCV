d17149fe16bd14c8126583e39eeaf6589c510020
george <khabazishvili20@gmail.com>
2023-05-24 19:35:17 +0400

    link

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -11,7 +11,7 @@ function Portfolio () {
                     <a href="mailto:khabazishvili20@gmail.com">khabazishvili20@gmail.com</a>
                 </div>
                 <div className='proj_link'>
-                    <a href="https://the-weather-2w3m.onrender.com/" target="_blank">my project</a>
+                    <a href="https://the-weather-2w3m.onrender.com/">my project</a>
                 </div>
             </div>
 
